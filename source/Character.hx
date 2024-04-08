2993ff63b3c3b5a648b1dd479b0c3a9c43374a7e
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-04-07 23:53:37 -0400

    fix

--- source/Character.hx
+++ source/Character.hx
@@ -207,7 +207,7 @@ class Character extends FlxSprite
 					updateHitbox();
 				}
 
-				usePlayerOffsets = json.isPlayer != null ? json.isPlayer : (character.startsWith('bf') || character.endsWith('-player') || character.endsWith('-playable'));
+				usePlayerOffsets = json.isPlayer != null ? json.isPlayer : (character.startsWith('bf') || character.endsWith('-player') || character.endsWith('playable'));
 				positionArray = json.position;
 				cameraPosition = json.camera_position;
 
