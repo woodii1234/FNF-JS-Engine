ef4b2e34253a3f1fba84fd4034fbd09516df551f
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-07-25 19:54:50 -0400

    uuguguggghhhh

--- setup/unix.sh
+++ setup/unix.sh
@@ -13,7 +13,6 @@ haxelib install flixel-ui
 haxelib install hxCodec 2.5.1
 haxelib install hscript
 haxelib install hxcpp-debug-server
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp/
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit master
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
