432e142d60c5bcbbcb5375d863caab805e1fdbdd
JordanSantiagoYT <61796448+JordanSantiagoYT@users.noreply.github.com>
2024-07-25 21:21:14 -0400

    Update unix.sh for the third time

--- setup/unix.sh
+++ setup/unix.sh
@@ -10,9 +10,9 @@ haxelib install flixel 5.6.2
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui	
-haxelib install hxCodec 2.5.1
 haxelib install hscript
 haxelib install hxcpp-debug-server
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
