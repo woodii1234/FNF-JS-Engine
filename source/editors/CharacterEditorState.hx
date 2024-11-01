862403f309cf5fe484fc59fa037e866a0a2d8607
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-11-01 16:00:25 -0400

    null ces fix
    closes #712

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -1116,7 +1116,7 @@ class CharacterEditorState extends MusicBeatState
 	function reloadCharacterOptions() {
 		if(UI_characterbox != null) {
 			imageInputText.text = char.imageFile;
-			noteskinText.text = char.noteskin;
+			if (char.noteskin != null) noteskinText.text = char.noteskin;
 			healthIconInputText.text = char.healthIcon;
 			vocalsInputText.text = char.vocalsFile != null ? char.vocalsFile : '';
 			singDurationStepper.value = char.singDuration;
