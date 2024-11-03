b9a447314e4537964f36c2445368412ccd23c36a
Jordan Santiago <61796448+JordanSantiagoYT@users.noreply.github.com>
2024-11-03 18:43:17 -0500

    dfgh mk,dfhjnm

--- setup/unix.sh
+++ setup/unix.sh
@@ -14,7 +14,7 @@ haxelib install hscript
 haxelib install hxcpp-debug-server
 haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp/
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
