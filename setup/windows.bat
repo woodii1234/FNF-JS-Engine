5543c844bfac9f1e13026cad6b1a9b6de2bb501d
UltimateQuack <patoflamejantetv@gmail.com>
2024-11-06 15:43:25 -0300

    Changes "@echo on" pos
    Small change lol

--- setup/windows.bat
+++ setup/windows.bat
@@ -1,9 +1,9 @@
 @echo off
 color 0a
 cd ..
-@echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
+@echo on
 haxelib install lime
 haxelib install openfl
 haxelib install flixel 5.6.2
@@ -19,5 +19,6 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+@echo off
 echo Finished!
 pause
