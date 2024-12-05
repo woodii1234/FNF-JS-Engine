6ca660f0d42058e71d7357d0903361299305b819
Moxie <124418090+moxie-coder@users.noreply.github.com>
2024-12-05 03:57:18 -0500

    Lily wanted this

--- source/StartupState.hx
+++ source/StartupState.hx
@@ -46,78 +46,77 @@ class StartupState extends MusicBeatState
 		FlxTween.tween(skipTxt, {alpha: 1}, 1);
 
 		new FlxTimer().start(0.1, function(tmr:FlxTimer) {
-			switch (theIntro) {
-				case 0:
-					FlxG.sound.play(Paths.sound('startup', 'splash'));
-					logo.scale.set(0.1,0.1);
-					logo.updateHitbox();
-					logo.screenCenter();
-					FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 0.95, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone()});
-				case 1:
-					FlxG.sound.play(Paths.sound('startup', 'splash'));
-					FlxG.sound.play(Paths.sound('FIREINTHEHOLE', 'splash'));
-					logo.loadGraphic(Paths.image('lobotomy', 'splash'));
-					logo.scale.set(0.1,0.1);
-					logo.updateHitbox();
-					logo.screenCenter();
-					FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 1.35, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone()});
-				case 2:
-					FlxG.sound.play(Paths.sound('screwedEngine', 'splash'));
-					logo.loadGraphic(Paths.image('ScrewedLogo', 'splash'));
-					logo.scale.set(0.1,0.1);
-					logo.updateHitbox();
-					logo.screenCenter();
-					FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 1.35, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone(0.6)});
-				case 3:
-					// secret muaahahhahhahaahha
-					FlxG.sound.play(Paths.sound('tada', 'splash'));
-					logo.loadGraphic(Paths.image('JavaScriptLogo', 'splash'));
-					logo.scale.set(0.1,0.1);
-					logo.updateHitbox();
-					logo.screenCenter();
-					FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 1.35, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone(0.6)});
-				case 4:
-					#if VIDEOS_ALLOWED
-						var vidSprite = new MP4Handler(); // it plays but it doesn't show???
-						#if (hxCodec < "3.0.0")
-						vidSprite.playVideo(Paths.video('bambiStartup'), false, false);
-						vidSprite.finishCallback = function()
-						{
-							try { vidSprite.dispose(); }
-							catch (e) {}
-							FlxG.switchState(TitleState.new);
-						};
-						#else
-						vidSprite.play(Paths.video('bambiStartup'));
-						vidSprite.onEndReached.add(function(){
-							vidSprite.dispose();
-							FlxG.switchState(TitleState.new);
-						});
-						#end
-					#end
-				case 5:
-					#if VIDEOS_ALLOWED
-						var vidSprite = new MP4Handler(); // it plays but it doesn't show???
-						#if (hxCodec < "3.0.0")
-						vidSprite.playVideo(Paths.video('broCopiedDenpa'), false, false);
-						vidSprite.finishCallback = function()
-						{
-							try { vidSprite.dispose(); }
-							catch (e) {}
-							FlxG.switchState(TitleState.new);
-						};
-						#else
-						vidSprite.play(Paths.video('broCopiedDenpa'));
-						vidSprite.onEndReached.add(function(){
-							vidSprite.dispose();
-							FlxG.switchState(TitleState.new);
-						});
-						#end
-					#end
+			if (!FlxG.random.bool(0.1)){
+				switch (theIntro) {
+					case 0:
+						FlxG.sound.play(Paths.sound('startup', 'splash'));
+						logo.scale.set(0.1,0.1);
+						logo.updateHitbox();
+						logo.screenCenter();
+						FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 0.95, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone()});
+					case 1:
+						FlxG.sound.play(Paths.sound('startup', 'splash'));
+						FlxG.sound.play(Paths.sound('FIREINTHEHOLE', 'splash'));
+						logo.loadGraphic(Paths.image('lobotomy', 'splash'));
+						logo.scale.set(0.1,0.1);
+						logo.updateHitbox();
+						logo.screenCenter();
+						FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 1.35, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone()});
+					case 2:
+						FlxG.sound.play(Paths.sound('screwedEngine', 'splash'));
+						logo.loadGraphic(Paths.image('ScrewedLogo', 'splash'));
+						logo.scale.set(0.1,0.1);
+						logo.updateHitbox();
+						logo.screenCenter();
+						FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 1.35, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone(0.6)});
+					case 3:
+						// secret muaahahhahhahaahha
+						FlxG.sound.play(Paths.sound('tada', 'splash'));
+						logo.loadGraphic(Paths.image('JavaScriptLogo', 'splash'));
+						logo.scale.set(0.1,0.1);
+						logo.updateHitbox();
+						logo.screenCenter();
+						FlxTween.tween(logo, {alpha: 1, "scale.x": 1, "scale.y": 1}, 1.35, {ease: FlxEase.expoOut, onComplete: _ -> onIntroDone(0.6)});
+					case 4:
+						playVideo('bambiStartup');
+					case 5:
+						playVideo('broCopiedDenpa');
+				}
 			}
+			else
+				playVideo('haxe'); // Lily wanted this
 		});
 
 		super.create();
+	}
+
+	// shorter & cleaner code = better :3
+	private function playVideo(name:String, ?callback:Void->Void):Void
+	{
+		#if VIDEOS_ALLOWED
+			var vidSprite = new MP4Handler(); // it plays but it doesn't show???
+			#if (hxCodec < "3.0.0")
+			vidSprite.playVideo(Paths.video(name), false, false);
+			vidSprite.finishCallback = function()
+			{
+				try { vidSprite.dispose(); }
+				catch (e) {}
+				if (callback != null)
+					callback();
+				else
+					FlxG.switchState(TitleState.new);
+			};
+			#else
+			vidSprite.play(Paths.video(name));
+			vidSprite.onEndReached.add(function(){
+				vidSprite.dispose();
+				if (callback != null)
+					callback();
+				else
+					FlxG.switchState(TitleState.new);
+			});
+			#end
+		#end
 	}
 
 	function onIntroDone(?fadeDelay:Float = 0) {
