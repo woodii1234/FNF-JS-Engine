fd2799f9eb3f444546f2b1f2bbb5be86a5077405
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-01-12 11:03:43 -0300

    Moxie did a oopsie

--- setup/unix.sh
+++ setup/unix.sh
@@ -19,5 +19,5 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
-haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc
+haxelib install hxvlc 1.9.2
 echo Finished!
--- setup/windows.bat
+++ setup/windows.bat
@@ -19,7 +19,7 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
-haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc
+haxelib install hxvlc 1.9.2
 @echo off
 echo Finished!
 pause
