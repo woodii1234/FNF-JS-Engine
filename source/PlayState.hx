3219d00de637dc3d09b5d4b9c617888f7f77323d
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-01-20 12:46:03 -0300

    commenting

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1334,17 +1334,17 @@ class PlayState extends MusicBeatState
 			case 'Forever Engine': 
 				EngineWatermark.text = "JS Engine v" + MainMenuState.psychEngineJSVersion;
 				EngineWatermark.x = FlxG.width - EngineWatermark.width - 5;
-				if (ClientPrefs.downScroll) EngineWatermark.y = healthBar.y + 50;
+				/*if (ClientPrefs.downScroll) EngineWatermark.y = healthBar.y + 50;
 				else {
 					return; // replace if wrong
-				}
+				}*/
 			case 'JS Engine': 
 				if (!ClientPrefs.downScroll) EngineWatermark.y = FlxG.height * 0.1 - 70;
 				EngineWatermark.text = "Playing " + SONG.song + " on " + CoolUtil.difficultyString() + " - JSE v" + MainMenuState.psychEngineJSVersion;
-				if (ClientPrefs.downScroll) EngineWatermark.y = healthBar.y + 50;
+				/*if (ClientPrefs.downScroll) EngineWatermark.y = healthBar.y + 50;
 				else {
 					return; // replace if wrong
-				}
+				}*/
 			case 'Dave Engine':
 				EngineWatermark.setFormat(Paths.font("comic.ttf"), 16, FlxColor.WHITE, RIGHT, OUTLINE,FlxColor.BLACK);
 				EngineWatermark.text = SONG.song;
