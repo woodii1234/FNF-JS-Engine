bb6fba776c372d7b2466baef0c2f69e65614c701
Stinko <145304742+TheStinkern@users.noreply.github.com>
2025-02-27 19:32:09 -0500

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -189,6 +189,7 @@ class ClientPrefs { //default settings if it can't find a save file containing y
 
 	//Misc
 	public static var JSEngineRecharts:Bool = false;
+	public static var SAM:Bool = false;
 	public static var alwaysTriggerCutscene:Bool = false;
 	public static var disableSplash:Bool = false;
 	public static var rainbowTime:Float = 5.0;
